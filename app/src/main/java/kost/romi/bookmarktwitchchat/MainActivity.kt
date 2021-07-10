ca2565ed82c58a96da812196f16fa0dfd78b3028
Romi <rominexus91@gmail.com>
2021-07-10 15:20:29 +0700

    Added navigation component.

--- app/src/main/java/kost/romi/bookmarktwitchchat/MainActivity.kt
+++ app/src/main/java/kost/romi/bookmarktwitchchat/MainActivity.kt
@@ -3,6 +3,13 @@ package kost.romi.bookmarktwitchchat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * TODO: add Navigation Component
+ * TODO: add Timber
+ * TODO: add new main empty Fragment
+ * TODO: add Tinder Scarlet
+ * TODO: IMPORTANT TO ALWAYS COMMIT AND PUSH AFTER TODO'S ARE DONE
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
