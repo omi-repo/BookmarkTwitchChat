266bf396db5b48125c4ce2aaa4f504f30398d23a
Romi <rominexus91@gmail.com>
2021-07-10 15:24:36 +0700

    Added Timber for logs.

--- app/src/main/java/kost/romi/bookmarktwitchchat/MainActivity.kt
+++ app/src/main/java/kost/romi/bookmarktwitchchat/MainActivity.kt
@@ -2,10 +2,9 @@ package kost.romi.bookmarktwitchchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 /**
- * TODO: add Navigation Component
- * TODO: add Timber
  * TODO: add new main empty Fragment
  * TODO: add Tinder Scarlet
  * TODO: IMPORTANT TO ALWAYS COMMIT AND PUSH AFTER TODO'S ARE DONE
@@ -14,5 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
